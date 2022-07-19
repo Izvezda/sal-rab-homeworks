7f23ef3abc9b74bf4d463f1bc667647c36af9ecc
Irina Zvezdina <Tar_irina@mail.ru>
2022-07-19 22:41:59 +0400

    "task4"

--- task_4/solution.js
+++ task_4/solution.js
@@ -15,6 +15,7 @@ function parseProducts(json) {
 function renderProuctsards(json) {
     clearProducts ();
     let products = parseProducts(json);
+    length = products.length;
     for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
      }
